d309413ff7d5e9ad7f62f6a4b42d1872866cbe60
MLankaoui <marouanelankaoui66@gmail.com>
2023-09-07 08:10:13 +0100

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,6 @@
-/* the preprocessor directives */
+/** the preprocessor directives
+ *
+ */
 #include <stdio.h>
 
 int main (void)
@@ -6,3 +8,10 @@ int main (void)
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (0);
 }
+/**
+ *
+ *
+ *printf to printf what inside of it
+ *
+ * return zero returns zero if the program runs smouthl
+ */
