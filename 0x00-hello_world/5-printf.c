e1922659a73949cf8a97e7a1e5a7e6286f9da26b
MLankaoui <marouanelankaoui66@gmail.com>
2023-09-07 06:30:04 +0100

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/*the preprocessor of our porgram*/
+#include <stdio.h>
+/**
+ * main - this is our main program function
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
