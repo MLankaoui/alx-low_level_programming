91941962a41fda29616cac0e314472b772850dc4
MLankaoui <marouanelankaoui66@gmail.com>
2023-09-07 06:24:07 +0100

    Hello, puts

---
+++ 0x00-hello_world/4-puts.C
@@ -0,0 +1,8 @@
+/**
+ *main - this is the main function program
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");/*it is printing the following
+	return (0)/*if the program work smouthly return 0*/
+}
