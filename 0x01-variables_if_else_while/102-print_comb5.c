f22433b8d0f7feaca8728e1923e2392726fa0e60
MLankaoui <marouanelankaoui66@gmail.com>
2023-09-08 20:43:24 +0100

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,5 +7,26 @@
  */
 int main(void)
 {
+	int n, m;
+
+	for (n = 0 ; n <= 99 ; n++)
+	{
+		for (m = 0 ; m <= 99 ; m++)
+		{
+			if (n < m && n != m)
+			{
+				putchar((n / 10) + '0');
+				putchar((n % 10) + '0');
+				putchar(' ');
+				putchar((m / 10) + '0');
+				putchar((m % 10) + '0');
+				if (m != 99 || n != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
 	return (0);
 }
