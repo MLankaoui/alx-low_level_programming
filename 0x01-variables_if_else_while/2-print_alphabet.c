8503fc18dfa4062a447fa82a538834d6a02dba6d
MLankaoui <marouanelankaoui66@gmail.com>
2023-09-08 11:23:12 +0100

    #include <stdio.h>

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	char alphabet[30] = "abcdefghijklmnopqrstuvwxyz";
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
 
 	for (int i = 0; alphabet != "\0"; i++)
 		putchar(alphabet[i]);
