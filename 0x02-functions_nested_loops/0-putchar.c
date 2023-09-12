c5e41628b9bb3b0ef699e9d9652fc7c687f9f99d
MLankaoui <marouanelankaoui66@gmail.com>
2023-09-12 20:58:19 +0100

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main -  prints _putchar, followed by a new line
+ * _putchar: print the alphabit putchar
+ * Return: always 0
+ */
 int main(void)
-{   
-    char putchar_name[8] = "_putchar";
-    int i;
-
-    for (i = 0; i < 8; i++)
-    {
-        putchar(putchar_name[i]);
-    }
-    putchar('\n');
-    return (0);
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
