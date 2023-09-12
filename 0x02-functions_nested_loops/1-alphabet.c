5b122a8bb8f775e8db30275789eec2f7f7f31d0b
MLankaoui <marouanelankaoui66@gmail.com>
2023-09-12 17:39:10 +0100

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
-/*
-*main - Entry point
-*
-*Return: Always 0 (Success)
-*/
+/* Function prototype */
+void print_alphabet(void);
+
 int main(void)
-{   
+{
     print_alphabet();
     return (0);
 }
@@ -16,16 +13,17 @@ int main(void)
 void print_alphabet(void)
 {
     char alpha[] = "abcdefghijkl";
-    char ALPHA[] = "mnopqrstuvwxyz";
+    char ALPHA[] = "MNOPQRSTUVWXYZ"; // Corrected uppercase letters
+    
     int i;
 
-    for (i = 0 ; i < 12 ; i++)
+    for (i = 0; i < 12; i++)
     {
         putchar(alpha[i]);
     }
-    for (i = 0 ; i < 14 ; i++)
+    for (i = 0; i < 14; i++)
     {
         putchar(ALPHA[i]);
     }
-
+    putchar('\n'); // Add a newline character to separate the output from the shell prompt.
 }
