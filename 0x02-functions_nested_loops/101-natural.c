a47a1292c8cb5ebf42319c30281b86781266f9ab
MLankaoui <marouanelankaoui66@gmail.com>
2023-09-12 21:30:21 +0100

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -19,13 +19,12 @@ int main(void)
         if ((i % 3) == 0)
         {
             sum3 = sum3 + i;
-        }
-        else if ((i % 5) == 0)
+        } else if ((i % 5) == 0)
         {
             sum5 = sum5 + i;
         }
     }
     sum = sum3 + sum5;
     printf("%lu\n", sum);
-    return (0);  
+    return (0);
 }
