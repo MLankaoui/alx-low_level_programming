895646efd9c7b46bd9cd3884ed0f0a86b38576d1
MLankaoui <marouanelankaoui66@gmail.com>
2023-09-13 14:20:00 +0100

    0>972

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,11 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a - b > 0 && b - c > 0)
 	{
 		largest = a;
 	}
-	else if (b > a  && a > c)
+	else if (b - a > 0  && a - c > 0)
 	{
 		largest = b;
 	}
