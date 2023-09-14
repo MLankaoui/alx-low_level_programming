90951c859080b619f54547e083da69748118c373
MLankaoui <marouanelankaoui66@gmai.com>
2023-09-14 14:02:01 +0100

    isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char c);
+void print_most_numbers(void);
+void more_numbers(void);
+
+#endif /* MAIN_H */
