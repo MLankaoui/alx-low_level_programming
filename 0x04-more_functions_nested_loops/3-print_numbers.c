d310d9cc06b1670c8f201295053b4b6b585d313a
MLankaoui <marouanelankaoui66@gmai.com>
2023-09-14 14:20:56 +0100

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,44 +1,36 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * _putchar - Writes a character to standard output.
+ * @c: The character to be written.
  *
- * Return: Always 0.
+ * Return: On success, returns the character written; on error, returns -1.
  */
-int main(void)
-{
-    print_numbers();
-    return (0);
-}
 int _putchar(char c)
 {
     return write(1, &c, 1);
 }
 
+/**
+ * print_numbers - Prints numbers from 0 to 9 followed by a dollar sign.
+ */
 void print_numbers(void)
 {
     int num = 0;
 
     do
-    {	
-    	if(num !=9)
-    	{
-    		_putchar(num + '0');
-        	num++;
-    	}
-    	else
-    	{
-    		_putchar(num + '0');
-    		printf("$");
-    		num++;
-    	}
-        
+    {
+        if (num != 9)
+        {
+            _putchar(num + '0');
+            num++;
+        }
+        else
+        {
+            _putchar(num + '0');
+            _putchar('$');
+            num++;
+        }
     }
     while (num <= 9);
 }
-
-
-
-
-
-
