a84625de6377f6d46cd12c521ddd88ff584f68d0
MLankaoui <marouanelankaoui66@gmai.com>
2023-09-14 14:50:29 +0100

    numbers speak for themselves

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,14 +5,14 @@
  */
 void print_most_numbers(void)
 {
-    int num = 0;
+	int num = 0;
 
-    while (num <= 9)
-    {
-        if (num != 2 && num != 4)
-        {
-            _putchar(num + '0');
-        }
-        num++;
-    }
+	while (num <= 9)
+	{
+		if (num != 2 && num != 4)
+		{
+			_putchar(num + '0');
+		}
+	num++;
+	}
 }
