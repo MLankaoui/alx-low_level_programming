18f46e5af3cab00faff9bcb80923a73f4638e85e
MLankaoui <marouanelankaoui66@gmai.com>
2023-09-14 14:13:54 +0100

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,8 +2,12 @@
 #include "main.h"
 
 /**
- * isDigit - function test if the elemnt is a digit (0 through 9)
+ * _isDigit - function test if the elemnt is a digit (0 through 9)
+ * @c: is just taking the value out of c variable wich has been declared earlier in the main function
  * 
+ * Description: this function is just a regular function that run some tests in c argument
+ * section header: this is just the section header
+ * Return: 1 (Success)
  */
 int _isdigit(int c)
 {
