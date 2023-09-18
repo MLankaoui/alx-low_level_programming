825c8136d67db6162f415854b6a40da785cd3e15
MLankaoui <marouanelankaoui66@gmail.com>
2023-09-18 15:47:08 +0100

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,13 +1,31 @@
 #include "main.h"
+#include <ctype.h>
 
 int _atoi(char *s)
 {
-	int num = 0;
+    int result = 0; // Initialize the result variable
+    int sign = 1;   // Initialize the sign variable (positive by default)
 
-	int i;
+    // Skip leading whitespace characters
+    while (*s == ' ' || (*s >= '\t' && *s <= '\r')) {
+        s++;
+    }
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		num = num * 10 + (s[i] - 48);
-	}
+    // Check for an optional sign
+    if (*s == '-' || *s == '+') {
+        if (*s == '-') {
+            sign = -1; // Negative sign
+        }
+        s++;
+    }
+
+    // Process digits to build the result
+    while (*s >= '0' && *s <= '9') {
+        result = result * 10 + (*s - '0');
+        s++;
+    }
+
+    // Apply the sign
+    return result * sign;
 }
+
