df811ae194c0336928e6f4946fe093f223167e90
MLankaoui <marouanelankaoui66@gmail.com>
2023-09-18 04:57:30 +0100

    Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*swap_int - just give the b value to a and the a value to b
+*
+*@a:having the value of b
+*@b:having the value of a
+*/
+void swap_int (int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
