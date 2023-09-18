e9b057b5a73811b0c856498f77c3180782155172
MLankaoui <marouanelankaoui66@gmail.com>
2023-09-18 05:49:54 +0100

    Winning is only half of it. Having fun is the other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,6 +10,14 @@ void puts_half(char *str)
 	int length = strlen(str);
 	int start = length / 2; /* Start at the middle of the string*/
 
+	if (length % 2 == 0)
+	{
+		start = length / 2;
+	}
+	else
+	{
+		start = (length + 1) / 2;
+	}
 	while (str[start] != '\0')
 	{
 		putchar(str[start]);
