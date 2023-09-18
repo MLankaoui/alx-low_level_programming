b8ac392bd92f263924dd2f5484f51d9e14199276
MLankaoui <marouanelankaoui66@gmail.com>
2023-09-18 06:10:42 +0100

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,7 +6,7 @@
  * @src: The source string.
  * Return: A pointer to dest.
  */
-char *my_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 {
 	strcpy(dest, src);
 	return (dest);
