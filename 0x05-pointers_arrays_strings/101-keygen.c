38f591ada11e7299a351cf76de04df9e2b4b3c31
MLankaoui <marouanelankaoui66@gmail.com>
2023-09-18 06:31:13 +0100

    Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Function to generate a random uppercase letter
+char randomUpperCaseLetter() {
+	return 'A' + (rand() % 26);
+}
+
+// Function to generate a random lowercase letter
+char randomLowerCaseLetter() {
+	return 'a' + (rand() % 26);
+}
+
+// Function to generate a random digit
+char randomDigit() {
+	return '0' + (rand() % 10);
+}
+
+// Function to generate a random special character (e.g., !, @, #, etc.)
+char randomSpecialCharacter() {
+	char specialCharacters[] = "!@#$%^&*()-_=+[]{}|;:,.<>?";
+
+	return (specialCharacters[rand() % sizeof(specialCharacters)]);
+}
+
+/* Function to generate a random password based on given criteria*/
+void generateRandomPassword(char *password, int length) {
+
+	for (int i = 0; i < length; i++) 
+	{
+
+		int choice = rand() % 4;
+
+		switch (choice) {
+			case 0:
+				password[i] = randomUpperCaseLetter();
+				break;
+			case 1:
+				password[i] = randomLowerCaseLetter();
+				break;
+			case 2:
+				password[i] = randomDigit();
+				break;
+			case 3:
+				password[i] = randomSpecialCharacter();
+				break;
+		}
+	}
+	password[length] = '\0'; /* Null-terminate the password*/
+}
+
+int main(void)
+{
+	srand(time(NULL)); /* Seed the random number generator with current time*/
+
+	int passwordLength = 12; /* Change this to the desired password length */
+
+	char password[passwordLength + 1]; /* +1 for the null terminator*/
+
+	generateRandomPassword(password, passwordLength);
+
+	printf("Generated Password: %s\n", password);
+
+	return (0);
+}
