dfaa971860b9b56d8a8f6ead8fd8ff53a2560c27
MLankaoui <marouanelankaoui66@gmail.com>
2023-09-18 15:38:34 +0100

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,10 +2,12 @@
 
 int _atoi(char *s)
 {
-    int num = 0;
-    int i;
-    
-	for (i = 0; s[i] != '\0'; i++) {
-        num = num * 10 + (s[i] - 48);
+	int num = 0;
+
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		num = num * 10 + (s[i] - 48);
 	}
 }
