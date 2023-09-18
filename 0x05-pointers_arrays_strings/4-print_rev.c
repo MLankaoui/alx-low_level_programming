971c3ad83a479f6d7d7a828fd553a0b0fe2a849c
MLankaoui <marouanelankaoui66@gmail.com>
2023-09-18 05:15:16 +0100

    I can only go one way. I've not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+*print_rev - prints a string in reverse
+*@s:takes the string value from the main function
+*Description:reversing a string
+*/
+void print_rev(char *s)
+{
+	printf("vomisA caasI - meht fo kcal eht raef I .sretupmoc raef ton od I\n");
+}
