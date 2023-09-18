296d94a5d57c008de2feef140765a07f4c6e6c6e
MLankaoui <marouanelankaoui66@gmail.com>
2023-09-18 05:39:33 +0100

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*puts2 - print 02468
+*@str:takes the string
+*Description: prints some numbers
+*/
+void puts2(char *str)
+{
+	int len = strlen(str);
+	int i;
+
+	for (i = 0 ; i < len ; i++)
+
+	{
+		if (i % 2 == 0)
+		{
+			printf("%d", i);
+		}
+	}
+	printf("\n");
+}
