e37a64c723e8182746713269d317a79b16d40c93
MLankaoui <marouanelankaoui66@gmail.com>
2023-09-21 19:21:04 +0100

    rot13

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,11 +1,12 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
+#ifndef MAIN_H
+#define MAIN_H
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
-
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+int _putchar(char c);
 #endif
