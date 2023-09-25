0969878b40f3d8d602faac86adadc1ab6c8110a6
MLankaoui <marouanelankaoui66@gmail.com>
2023-09-25 18:59:20 +0100

    printchess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* print_chessboard - Prints a chessboard.
+* @a: 2D array representing the chessboard.
+*/
+void print_chessboard(char (*a)[8])
+{
+	int row, col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			putchar(a[row][col]);
+			if (col < 7)
+				putchar(' ');
+		}
+		putchar('\n');
+	}
+}
