12d5dc0e49e9ef7e32c0a5d18d4981169b42e49b
MLankaoui <marouanelankaoui66@gmail.com>
2023-10-03 06:57:25 +0100

    It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,52 +1,41 @@
-#include <stdlib.h>
+#include "main.h"
 
 /**
-* argstostr - Concatenate all arguments into a single string.
-* @ac: The number of arguments.
-* @av: An array of argument strings.
-*
-* Return: A pointer to the concatenated string, or NULL if it fails.
+* argstostr - main entry
+* @ac: int input
+* @av: double pointer array
+* Return: 0
 */
 char *argstostr(int ac, char **av)
 {
+	int i, n, r = 0, l = 0;
+
+	char *str;
+
 	if (ac == 0 || av == NULL)
-	{
-		return (NULL); /* If ac is 0 or av is NULL, return NULL */
-	}
+		return (NULL);
 
-	int total_length = 0;
-
-	int i, j, k = 0;
-
-	/* Calculate the total length of the concatenated string */
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			total_length++;
-		}
-		total_length++; /* Add 1 for the newline character */
+		for (n = 0; av[i][n]; n++)
+			l++;
 	}
+	l += ac;
 
-	/* Allocate memory for the concatenated string */
-	char *concatenated = malloc((total_length + 1) * sizeof(char));
-
-	if (concatenated == NULL)
-	{
-		return (NULL); /* Memory allocation failed, return NULL */
-	}
-
-	/* Copy each argument and add a newline character */
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
+		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		for (n = 0; av[i][n]; n++)
 		{
-			concatenated[k++] = av[i][j];
+			str[r] = av[i][n];
+			r++;
 		}
-		concatenated[k++] = '\n'; /* Add newline character */
+		if (str[r] == '\0')
+		{
+			str[r++] = '\n';
+		}
 	}
-
-	concatenated[k] = '\0'; /* Null-terminate the concatenated string */
-
-	return (concatenated);
+	return (str);
 }
