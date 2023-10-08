7af9ce0d80f84891d37f8114b6317b1f368b7481
MLankaoui <marouanelankaoui66@gmail.com>
2023-10-08 21:47:33 +0100

    string_nconcat

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,17 +1,18 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _calloc - allocates memory for an array.
- * @nmemb: number of elements.
- * @size: size of bytes.
- *
- * Return: pointer to the allocated memory.
- * if nmemb or size is 0, returns NULL.
- * if malloc fails, returns NULL.
- */
+* _calloc - allocates memory for an array.
+* @nmemb: number of elements.
+* @size: size of bytes.
+*
+* Return: pointer to the allocated memory.
+* if nmemb or size is 0, returns NULL.
+* if malloc fails, returns NULL.
+*/
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *p;
+
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
