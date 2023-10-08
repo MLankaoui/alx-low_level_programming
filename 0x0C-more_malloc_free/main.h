4a30086ba12b0db66dae01cb7829462a08ef05d9
MLankaoui <marouanelankaoui66@gmail.com>
2023-10-08 21:34:42 +0100

    Trust no one

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <limits.h>
+
+int _putchar(char c);
+
+void *malloc_checked(unsigned int b);
+
+#endif
