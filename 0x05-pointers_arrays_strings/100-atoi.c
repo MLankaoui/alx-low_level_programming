5deb363930b7da009994755b23474f32ceb4a087
MLankaoui <marouanelankaoui66@gmail.com>
2023-11-15 07:43:29 +0100

    Test commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,35 +1,42 @@
-#include "main.h"
-
 /**
- * _atoi - Convert a string to an integer
- * @s: The string to convert
+ * _atoi - Converts a string to an integer.
+ * @s: The input string to be converted.
  *
- * Return: The integer value of the string.
+ * Return: The integer value extracted from the string.
  */
 int _atoi(char *s)
 {
-	int nb;
-	int sign;
+	/* Initialize variables */
+	int nb;    /* Stores the final result. */
+	int sign;  /* Keeps track of the sign. */
 
+	nb = 0;    /* Initialize 'nb' to 0 (the initial result). */
+	sign = 1;  /* Initialize 'sign' to 1 (positive sign). */
 
-	nb = 0;
-	sign = 1;
+	/* Iterate through the input string 's' until the end is reached. */
 	while (*s)
 	{
-		if (*s == '-')
+		if (*s == '-')  /* Check if the current character is a minus sign. */
 		{
-			sign *= -1;
+			sign *= -1;  /* Toggle the sign by multiplying 'sign' by -1. */
 		}
-		else if (*s >= '0' && *s <= '9')
+		else if (*s >= '0' && *s <= '9')  /* Check if the current character is a digit (0-9). */
 		{
+			/* Update 'nb' by multiplying the current value of 'nb' by 10 and adding the digit to it,
+			 * considering the sign.
+			 */
 			nb = nb * 10 + sign * (*s - '0');
 		}
-		else if (nb != 0)
+		/* Check if 'nb' has already been assigned a non-zero value. */
+		else if (nb != 0)  
 		{
-			break;
+			/* Exit the loop to avoid further processing. */
+			break;  
 		}
-		s++;
+		/* Move to the next character in the input string. */
+		s++;  
 	}
 
+	/* Return the final integer result. */
 	return (nb);
 }
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,18 +11,18 @@ char *cap_string(char *s)
 	int i, j;
 
 	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
-		'!', '?', '"', '(', ')', '{', '}'};
+		'!', '?', '"', '(', ')', '{', '}'}; /* arry that has all the non text values possible*/
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)/*go from the beginnering to the end of the string*/
 	{
-		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')/*if this is the first element and that first element is between a and z s[i] - 32 just for uppercase*/
 			s[i] -= 32;
 
-		for (j = 0; j < 13; j++)
+		for (j = 0; j < 13; j++) /* a loop to iterate over the content of the array of non text*/
 		{
-			if (s[i] == spe[j])
+			if (s[i] == spe[j])/*if an elemnt is a non text*/
 			{
-				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')/*if the element after is between a and z capitalize*/
 				{
 					s[i + 1] -= 32;
 				}
