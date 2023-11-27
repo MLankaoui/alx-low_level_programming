95ab0e2080f204b8351d212c3e5f4807e3b1cddc
MLankaoui <marouanelankaoui66@gmail.com>
2023-11-27 10:13:16 +0100

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/**
+ * struct dog - A structure representing a dog with a name, age, and owner.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Description: This structure defines a dog with its name, age, and owner.
+ */
+struct dog
+{
+	char *name;  /* Name of the dog */
+	float age;   /* Age of the dog */
+	char *owner; /* Owner of the dog */
+};
+
+#endif
