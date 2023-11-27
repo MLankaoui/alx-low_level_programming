826c768c609ffdd5492054d68f088f45d7c543b0
MLankaoui <marouanelankaoui66@gmail.com>
2023-11-27 10:57:16 +0100

    Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,40 +2,44 @@
 #define HEADER_FILE
 
 /**
- * struct dog_t - A structure representing a dog with a name, age, and owner.
+ * struct dog - A structure representing a dog with a name, age, and owner.
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
  *
  * Description: This structure defines a dog with its name, age, and owner.
  */
-typedef struct dog_t
+struct dog
 {
 	char *name;  /* Name of the dog */
 	float age;   /* Age of the dog */
 	char *owner; /* Owner of the dog */
-} dog_t;
+};
 
 /**
- * init_dog - Initializes a variable of type struct dog_t.
- * @d: Pointer to the struct dog_t to be initialized.
+ * dog_t - Typedef for struct dog.
+ */
+typedef struct dog dog_t;
+
+/**
+ * init_dog - Initializes a variable of type struct dog.
+ * @d: Pointer to the struct dog to be initialized.
  * @name: Name to assign to the dog.
  * @age: Age to assign to the dog.
  * @owner: Owner to assign to the dog.
  *
- * Description: This function initializes a variable of type struct dog_t
+ * Description: This function initializes a variable of type struct dog
  * with the provided name, age, and owner.
  */
 void init_dog(dog_t *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
 
 /**
  * print_dog - Prints information about a dog.
- * @d: Pointer to the struct dog_t to be printed.
+ * @d: Pointer to the struct dog to be printed.
  *
  * Description: This function prints information about a dog,
  * including its name, age, and owner.
  */
-void print_dog(dog_t *d);
+void print_dog(const dog_t *d);
 
 #endif
