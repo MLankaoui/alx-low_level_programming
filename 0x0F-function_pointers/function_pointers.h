0dbd8c12b73ae987882fbbd5ee3aa27fde766b35
MLankaoui <marouanelankaoui66@gmail.com>
2023-11-29 17:53:58 +0100

    function pointers

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stddef.h>
+
+void print_name_as_is(char *name);
+void print_name_uppercase(char *name);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
