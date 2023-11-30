181944fc6800dc83bca4481d595975495f760ddf
MLankaoui <marouanelankaoui66@gmail.com>
2023-11-30 18:47:22 +0100

    One woman's constant is another woman's variable

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	if (separator == NULL || n == 0)
 	{
 		va_end(args);
-		return ;
+		return;
 	}
 	for (i = 0; i < n; i++)
 	{
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,28 +1,39 @@
 #include "variadic_functions.h"
 
+/**
+ * print_strings - Prints strings followed by a separator
+ * @separator: The string to be printed between strings
+ * @n: The number of strings to be printed
+ * @...: Variable number of strings to be printed
+ *
+ * Description: This function prints a variable number of strings separated
+ * by a specified string. If the separator is NULL or n is 0, the function
+ * does nothing.
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i;
-    const char *str;
+	va_list args;
+	unsigned int i;
 
-    va_start(args, n);
+	const char *str;
 
-    if (separator == NULL || n == 0)
-    {
-        va_end(args);
-        return;
-    }
+	va_start(args, n);
 
-    for (i = 0; i < n; i++)
-    {
-        str = va_arg(args, const char *);
-        printf("%s", str);
+	if (separator == NULL || n == 0)
+	{
+		va_end(args);
+		return;
+	}
 
-        if (i < n - 1 && separator != NULL)
-            printf("%s", separator);
-    }
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, const char *);
+		printf("%s", str);
 
-    va_end(args);
-    printf("\n");
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+
+	va_end(args);
+	printf("\n");
 }
