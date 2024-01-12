604b432c094d4c269622ea7f0a2edcf01a0843ba
MLankaoui <marouanelankaoui66@gmail.com>
2024-01-12 15:33:50 +0100

    Readme file added and first file created and compiled

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,9 @@
  *
  * Return: Always 0 (Success)
  */
-int main(__attribute__((unused)) int argc, char **argv)
+int main(__attribute__((unused)) int argc, char *argv[])
 {
-	printf("%s", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
 
