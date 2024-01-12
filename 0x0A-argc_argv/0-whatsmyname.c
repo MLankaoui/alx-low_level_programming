dc2a130a80243ea87609c4935cf420fd167c1d64
MLankaoui <marouanelankaoui66@gmail.com>
2024-01-12 15:31:19 +0100

    Readme file added and first file created and compiled

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(__attribute__((unused)) int argc, char **argv)
+{
+	printf("%s", argv[0]);
+	return (0);
+}
+
