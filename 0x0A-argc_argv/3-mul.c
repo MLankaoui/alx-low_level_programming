9c8e713226afd2f2f45ea6dfa222d22a4aa4328c
MLankaoui <marouanelankaoui66@gmail.com>
2024-01-12 17:37:36 +0100

    fourth task added and compiled

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* main - Entry point
+* @argc: The number of command-line arguments
+* @argv: An array containing the command-line arguments
+*
+* Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
