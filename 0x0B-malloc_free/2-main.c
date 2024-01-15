edab5c67e2721ed690c68fc18c4074f0dbb4606b
MLankaoui <marouanelankaoui66@gmail.com>
2024-01-15 11:22:35 +0100

    main.h updated 2-main.c added and third task tackled

--- 0x0B-malloc_free/2-main.c
+++ 0x0B-malloc_free/2-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 * main - check the code for ALX School students.
 *
@@ -8,10 +9,10 @@ int main(void)
 {
 	char *s;
 
-	s = _strdup("ALX SE");
+	s = str_concat("Betty ", "Holberton");
 	if (s == NULL)
 	{
-		printf("failed to allocate memory\n");
+		printf("failed\n");
 		return (1);
 	}
 	printf("%s\n", s);
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,9 +6,11 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 
 #endif
 
