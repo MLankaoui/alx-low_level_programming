9df51a98825728bb113da63858ee441a9bd7f690
MLankaoui <marouanelankaoui66@gmail.com>
2024-01-16 07:40:19 +0100

    main.h updated 4-main.c added and sixth task tackled

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ * free_grid - Frees the memory allocated for a 2D grid.
+ * @grid: The 2D grid to be freed.
+ * @height: The height of the grid.
+ *
+ * This function iterates through each row of the grid and frees the memory
+ * allocated for each row. Then, it frees the memory allocated for the grid
+ * itself.
+ */
 void free_grid(int **grid, int height)
 {
 	int i;
