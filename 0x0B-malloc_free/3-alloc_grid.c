252059df4fd63d392a76d2d41708978226a6aa9e
MLankaoui <marouanelankaoui66@gmail.com>
2024-01-16 07:27:42 +0100

    main.h updated 3-main.c added and fourth task tackled

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,9 +25,9 @@ int **alloc_grid(int width, int height)
 		grid[i] = (int *)malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
-			/*for (j = 0; j < i; j++)
+			for (j = 0; j < i; j++)
 				free(grid[j]);
-			free(grid);*/
+			free(grid);
 			return (NULL);
 		}
 	}
