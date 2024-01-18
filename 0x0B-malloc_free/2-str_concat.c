9457c4b817e5953bcdcd79db60014a20f07a54f2
MLankaoui <marouanelankaoui66@gmail.com>
2024-01-18 13:05:41 +0100

    header file updated and third task handled

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -26,28 +26,37 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 	}
 	lengths1 = 0;
+	/* to calculate the length of s1*/
 	while (s1[lengths1] != '\0')
 	{
 		lengths1++;
 	}
 	lengths2 = 0;
+	/* to calculate the length of s2*/
 	while (s2[lengths2] != '\0')
 	{
 		lengths2++;
 	}
+	/*allocation of the memory*/
 	conc = malloc((lengths1 + lengths2 + 1) * sizeof(char));
+	/*if insufficient or no allocation is done return NULL*/
 	if (conc == NULL)
 		return (NULL);
+	/*taking conc and initilize it with s1 content*/
 	for (i = 0; i < lengths1; i++)
 	{
 		conc[i] = s1[i];
 	}
+	/*concatenating it with content of s2 string*/
 	for (j = 0; j < lengths2; j++)
 	{
 		conc[i + j] = s2[j];
 	}
+	/*hard coding the null value after all of that*/
 	conc[i + j] = '\0';
+	/*returning the pointer*/
 	return (conc);
+	/*freeing everytinhg to prevent any leaks*/
 	free(conc);
 }
 
