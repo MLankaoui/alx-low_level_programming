dc4e18a2a4f0e672bc0ebb60da2922d003509455
MLankaoui <marouanelankaoui66@gmail.com>
2024-01-18 10:53:06 +0100

    Adding main.h and hadling the first task and compiling it

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * malloc_checked - Allocates memory using malloc
- * @b: The number of bytes to allocate
- *
- * Return: A pointer to the allocated memory
- *         Exits with status 98 if malloc fails
- */
+* malloc_checked - Allocates memory using malloc
+* @b: The number of bytes to allocate
+*
+* Return: A pointer to the allocated memory
+*         Exits with status 98 if malloc fails
+*/
 void *malloc_checked(unsigned int b)
 {
 	unsigned int *ptr;
 
-	ptr = malloc(sizeof b);
+
+	ptr = malloc(b);
 
 	if (ptr == NULL)
 	{
@@ -19,5 +20,4 @@ void *malloc_checked(unsigned int b)
 	}
 
 	return (ptr);
-	free(ptr);
 }
