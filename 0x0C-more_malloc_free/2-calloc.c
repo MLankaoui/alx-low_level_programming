f4f05cfa59b15da58cb327b961e088d3adf8f6f7
MLankaoui <marouanelankaoui66@gmail.com>
2024-01-18 12:11:11 +0100

    header file updated and third task handled

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int *ptr, i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(size * sizeof(unsigned int));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size ; i++)
+	{
+		ptr[i] = nmemb;
+	}
+
+	return (ptr);
+}
