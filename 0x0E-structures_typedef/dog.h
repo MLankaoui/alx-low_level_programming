fced04a29d03fc242d56afaf7cf203de188259b5
MLankaoui <marouanelankaoui66@gmail.com>
2024-01-22 14:05:13 +0100

    README file added and first header file created , and first task handled

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/**
+ * struct dog - A structure representing a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Description: This structure defines a dog with a name,
+ *              age, and owner's name.
+ */
+struct dog
+{
+	char *name;   /* The name of the dog */
+	float age;    /* The age of the dog */
+	char *owner;  /* The owner of the dog */
+};
+
+#endif /* HEADER_FILE */
+
