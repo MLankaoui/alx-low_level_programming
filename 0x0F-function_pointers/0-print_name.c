30000dc17d1090319daa88fa128f5733d821a31f
MLankaoui <marouanelankaoui66@gmail.com>
2024-01-24 16:22:05 +0100

    README.md added with the header file and first task handled

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,16 @@
 #include "function_pointers.h"
-
+/**
+* print_name - Prints a name using a function pointer.
+* @name: The name to be printed.
+* @f: A pointer to a function that takes a char* as a parameter
+* and returns void.
+*
+* Description: This function takes a name and a
+* function pointer as arguments.
+* It checks if the name and function pointer are not NULL,
+* and then calls the
+* function with the provided name as an argument.
+*/
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
