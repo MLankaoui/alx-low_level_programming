60180cdc7d47a4c943d793cd71d5a50451389465
MLankaoui <marouanelankaoui66@gmail.com>
2024-02-05 08:23:51 +0100

    fourth task compiled and betty fixed

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * add_nodeint_end - Adds a new node at the end of a listint_t list.
+ * @head: Pointer to the head of the list.
+ * @n: Value to be stored in the new node.
+ *
+ * Return: Address of the new element or NULL if it fails.
+ */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *temp, *newNode;
