94c0cdefd4f848062337b040226c6c06e7a48f27
MLankaoui <marouanelankaoui66@gmail.com>
2024-02-06 08:04:25 +0100

    betty fixed

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+* free_listint2 - Frees a linked list and sets the head to NULL
+* @head: Pointer to the pointer to the first node of the list
+*
+* This function frees all the nodes in a linked list and sets the head
+* pointer to NULL.
+*/
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
