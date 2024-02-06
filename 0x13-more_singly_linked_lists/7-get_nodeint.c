3e1811fff62c069c0820b0eaaffc27f7028dfca8
MLankaoui <marouanelankaoui66@gmail.com>
2024-02-06 08:49:19 +0100

    8th task handled

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,6 +12,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i = 0;
 	listint_t *current = head;
 
+	if (head == NULL)
+		return (NULL);
+
 	while (current != NULL && i < index)
 	{
 		current = current->next;
