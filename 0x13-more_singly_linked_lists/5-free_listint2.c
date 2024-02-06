cdd92864a96f10ac9b241dcbf1dd202082fb1698
MLankaoui <marouanelankaoui66@gmail.com>
2024-02-06 07:58:29 +0100

    everything up to date

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+}
