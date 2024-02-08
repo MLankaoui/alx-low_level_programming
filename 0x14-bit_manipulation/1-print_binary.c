c0bcbf2aa0c7a88ee5481019083db16f2886c535
MLankaoui <marouanelankaoui66@gmail.com>
2024-02-08 18:24:34 +0100

    betty fixed

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,9 @@
 #include "main.h"
 
+/**
+ * print_binary - Prints the binary representation of a number
+ * @n: The number to print in binary
+ */
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
