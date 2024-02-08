2388b0d3585ee139557915e4424ea11e6034feae
MLankaoui <marouanelankaoui66@gmail.com>
2024-02-08 18:22:12 +0100

    task 1 last attempt

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,28 +2,8 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int temp = n;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	int size = 0, i;
-
-	if (n == 0)
-	{
-		printf("0\n");
-		return;
-	}
-
-	while (temp > 0)
-	{
-		temp = temp >> 1;
-		size++;
-	}
-
-	for (i = size - 1; i >= 0; i--)
-	{
-		if ((n >> i) & 1)
-			printf("1");
-		else
-			printf("0");
-	}
-	printf("\n");
+	_putchar((n & 1) + '0');
 }
