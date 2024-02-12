16c73ef5c8fa5f46e9d5071d4db679873a898233
MLankaoui <marouanelankaoui66@gmail.com>
2024-02-12 10:42:40 +0100

    first try task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,7 +12,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
         return 0;
     }
 
-    ptr = fopen(filename, "r");
+    ptr = fopen(filename, O_RDONLY);
 
     if (ptr == NULL)
     {
