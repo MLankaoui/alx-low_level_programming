459f05f8578b27a149610d59a1e4196b5ef23498
MLankaoui <marouanelankaoui66@gmail.com>
2024-02-12 11:26:45 +0100

    task 3

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,12 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
-#include <stddef.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
