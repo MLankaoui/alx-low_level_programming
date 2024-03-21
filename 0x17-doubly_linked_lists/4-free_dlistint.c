0c66415ea42f7257099faeac03e89f5671f2a356
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-21 11:50:03 +0000

    task 4 betty fixed

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,11 +1,17 @@
 #include "lists.h"
 
+/**
+ * free_dlistint - Frees a dlistint_t list
+ * @head: A pointer to the head of the dlistint_t list
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp = head;
-	while (tmp!= NULL)
+
+	while (tmp != NULL)
 	{
 		dlistint_t *next = tmp->next;
+
 		free(tmp);
 		tmp = next;
 	}
