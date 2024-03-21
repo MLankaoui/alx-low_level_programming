e8253f4c01db385b90d672984815628d907d4940
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-21 10:29:23 +0000

    header file added

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef LIST
+#define LIST
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * 
+ */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#endif
