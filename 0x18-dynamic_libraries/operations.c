4233ed3f4b88785bb7aa91ffbfa3b827f48a56fd
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-25 11:49:32 +0000

    adding operations

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,37 @@
+int add(int a, int b)
+{
+	return (a + b);
+}
+int sub(int a, int b)
+{
+	return (a - b);
+}
+int mul(int a,int b)
+{
+	return (a * b);
+}
+
+int div(int a, int b)
+{
+	if (b == 0)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (a / b);
+	}
+}
+int mod(int a, int b)
+{
+	if (b == 0)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (a % b);
+	}
+}
